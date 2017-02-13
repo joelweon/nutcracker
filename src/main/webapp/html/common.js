c2a907af363ec0f12e6f3586a2bb96123b57f59e
hanokjoo <okjoo.han88@gmail.com>
2017-02-13 14:10:31 +0900

    .

---
+++ src/main/webapp/html/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#wrap-header').load('./header.html');
+});
+
+/*-----------------------------
+ * 메인 JavaScript
+ -----------------------------*/
+//불매 불량 바로가기
+function fnMove(seq){
+    var offset = $("#title-bar-" + seq).offset();
+    $('html, body').animate({scrollTop : offset.top}, 400);
+};
