4f7405aa92152995738c92c10d9bf48fc2b77a1d
hanokjoo <okjoo.han88@gmail.com>
2017-02-17 10:34:06 +0900

    DB 댓글 샘플데이터 파일 추가(comment.sql)

---
+++ dbmodel/comment.sql
@@ -0,0 +1,145 @@
+--댓글
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(600,2,'2017-02-17 04:04:04.0','1번 불량후기댓글입니다',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(601,3,'2017-02-17 04:04:04.0','2번 불량후기댓글입니다',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(602,4,'2017-02-17 04:04:04.0','3번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(603,5,'2017-02-17 04:04:04.0','4번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(604,6,'2017-02-17 04:04:04.0','5번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(605,7,'2017-02-17 04:04:04.0','6번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(606,8,'2017-02-17 04:04:04.0','7번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(607,9,'2017-02-17 04:04:04.0','8번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(608,10,'2017-02-17 04:04:04.0','9번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(609,11,'2017-02-17 04:04:04.0','10번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(610,2,'2017-02-17 04:04:04.0','11번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(611,3,'2017-02-17 04:04:04.0','12번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(612,4,'2017-02-17 04:04:04.0','13번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(613,5,'2017-02-17 04:04:04.0','14번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(614,6,'2017-02-17 04:04:04.0','15번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(615,7,'2017-02-17 04:04:04.0','16번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(616,8,'2017-02-17 04:04:04.0','17번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(617,9,'2017-02-17 04:04:04.0','18번 불량후기댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(618,10,'2017-02-17 04:04:04.0','19번 불량후기댓글입니다',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(619,11,'2017-02-17 04:04:04.0','20번 불량후기댓글입니다',1,'n');
+
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(620,2,'2017-02-17 04:04:04.0','1번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(621,3,'2017-02-17 04:04:04.0','2번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(622,4,'2017-02-17 04:04:04.0','3번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(623,5,'2017-02-17 04:04:04.0','4번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(624,6,'2017-02-17 04:04:04.0','5번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(625,7,'2017-02-17 04:04:04.0','6번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(626,8,'2017-02-17 04:04:04.0','7번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(627,9,'2017-02-17 04:04:04.0','8번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(628,10,'2017-02-17 04:04:04.0','9번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(629,11,'2017-02-17 04:04:04.0','10번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(630,2,'2017-02-17 04:04:04.0','11번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(631,3,'2017-02-17 04:04:04.0','12번 불매운동댓글입니다',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(632,4,'2017-02-17 04:04:04.0','13번 불매운동댓글입니다',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(633,5,'2017-02-17 04:04:04.0','14번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(634,6,'2017-02-17 04:04:04.0','15번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(635,7,'2017-02-17 04:04:04.0','16번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(636,8,'2017-02-17 04:04:04.0','17번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(637,9,'2017-02-17 04:04:04.0','18번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(638,10,'2017-02-17 04:04:04.0','19번 불매운동댓글입니다',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(639,11,'2017-02-17 04:04:04.0','20번 불매운동댓글입니다',0,'n');
+
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(640,2,'2017-02-17 04:04:04.0','1번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(641,3,'2017-02-17 04:04:04.0','2번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(642,4,'2017-02-17 04:04:04.0','3번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(643,5,'2017-02-17 04:04:04.0','4번 공동구매상품평입니다.',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(644,6,'2017-02-17 04:04:04.0','5번 공동구매상품평입니다.',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(645,7,'2017-02-17 04:04:04.0','6번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(646,8,'2017-02-17 04:04:04.0','7번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(647,9,'2017-02-17 04:04:04.0','8번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(648,10,'2017-02-17 04:04:04.0','9번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(649,11,'2017-02-17 04:04:04.0','10번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(650,2,'2017-02-17 04:04:04.0','11번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(651,3,'2017-02-17 04:04:04.0','12번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(652,4,'2017-02-17 04:04:04.0','13번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(653,5,'2017-02-17 04:04:04.0','14번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(654,6,'2017-02-17 04:04:04.0','15번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(655,7,'2017-02-17 04:04:04.0','16번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(656,8,'2017-02-17 04:04:04.0','17번 공동구매상품평입니다.',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(657,9,'2017-02-17 04:04:04.0','18번 공동구매상품평입니다.',1,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(658,10,'2017-02-17 04:04:04.0','19번 공동구매상품평입니다.',0,'n');
+insert into cmt(ctno,mno,ct_date,cont,rep_cnt,del_yn) values(659,11,'2017-02-17 04:04:04.0','20번 공동구매상품평입니다.',0,'n');
+
+
+--불량운동댓글
+insert into rev_cmt(ctno,rvno) values(600,200);
+insert into rev_cmt(ctno,rvno) values(601,200);
+insert into rev_cmt(ctno,rvno) values(602,201);
+insert into rev_cmt(ctno,rvno) values(603,201);
+insert into rev_cmt(ctno,rvno) values(604,202);
+insert into rev_cmt(ctno,rvno) values(605,202);
+insert into rev_cmt(ctno,rvno) values(606,203);
+insert into rev_cmt(ctno,rvno) values(607,203);
+insert into rev_cmt(ctno,rvno) values(608,204);
+insert into rev_cmt(ctno,rvno) values(609,204);
+insert into rev_cmt(ctno,rvno) values(610,205);
+insert into rev_cmt(ctno,rvno) values(611,205);
+insert into rev_cmt(ctno,rvno) values(612,206);
+insert into rev_cmt(ctno,rvno) values(613,206);
+insert into rev_cmt(ctno,rvno) values(614,207);
+insert into rev_cmt(ctno,rvno) values(615,207);
+insert into rev_cmt(ctno,rvno) values(616,208);
+insert into rev_cmt(ctno,rvno) values(617,208);
+insert into rev_cmt(ctno,rvno) values(618,209);
+insert into rev_cmt(ctno,rvno) values(619,209);
+
+
+--불매운동댓글
+insert into bot_cmt(ctno,botno) values(620,300);
+insert into bot_cmt(ctno,botno) values(621,300);
+insert into bot_cmt(ctno,botno) values(622,301);
+insert into bot_cmt(ctno,botno) values(623,301);
+insert into bot_cmt(ctno,botno) values(624,302);
+insert into bot_cmt(ctno,botno) values(625,302);
+insert into bot_cmt(ctno,botno) values(626,303);
+insert into bot_cmt(ctno,botno) values(627,303);
+insert into bot_cmt(ctno,botno) values(628,304);
+insert into bot_cmt(ctno,botno) values(629,304);
+insert into bot_cmt(ctno,botno) values(630,305);
+insert into bot_cmt(ctno,botno) values(631,305);
+insert into bot_cmt(ctno,botno) values(632,306);
+insert into bot_cmt(ctno,botno) values(633,306);
+insert into bot_cmt(ctno,botno) values(634,307);
+insert into bot_cmt(ctno,botno) values(635,307);
+insert into bot_cmt(ctno,botno) values(636,308);
+insert into bot_cmt(ctno,botno) values(637,308);
+insert into bot_cmt(ctno,botno) values(638,309);
+insert into bot_cmt(ctno,botno) values(639,309);
+
+
+--공동구매상품평
+insert into pch_cmt(ctno,pno) values(640,400);
+insert into pch_cmt(ctno,pno) values(641,400);
+insert into pch_cmt(ctno,pno) values(642,401);
+insert into pch_cmt(ctno,pno) values(643,401);
+insert into pch_cmt(ctno,pno) values(644,402);
+insert into pch_cmt(ctno,pno) values(645,402);
+insert into pch_cmt(ctno,pno) values(646,403);
+insert into pch_cmt(ctno,pno) values(647,403);
+insert into pch_cmt(ctno,pno) values(648,404);
+insert into pch_cmt(ctno,pno) values(649,404);
+insert into pch_cmt(ctno,pno) values(650,405);
+insert into pch_cmt(ctno,pno) values(651,405);
+insert into pch_cmt(ctno,pno) values(652,406);
+insert into pch_cmt(ctno,pno) values(653,406);
+insert into pch_cmt(ctno,pno) values(654,407);
+insert into pch_cmt(ctno,pno) values(655,407);
+insert into pch_cmt(ctno,pno) values(656,408);
+insert into pch_cmt(ctno,pno) values(657,408);
+insert into pch_cmt(ctno,pno) values(658,409);
+insert into pch_cmt(ctno,pno) values(659,409);
+
+
+--댓글신고
+insert into cmt_rep(ctno,mno,rwno) values(600,2,700);
+insert into cmt_rep(ctno,mno,rwno) values(601,3,701);
+insert into cmt_rep(ctno,mno,rwno) values(618,10,702);
+insert into cmt_rep(ctno,mno,rwno) values(619,11,703);
+insert into cmt_rep(ctno,mno,rwno) values(631,3,704);
+insert into cmt_rep(ctno,mno,rwno) values(632,4,705);
+insert into cmt_rep(ctno,mno,rwno) values(643,5,706);
+insert into cmt_rep(ctno,mno,rwno) values(644,6,707);
+insert into cmt_rep(ctno,mno,rwno) values(656,8,708);
+insert into cmt_rep(ctno,mno,rwno) values(657,9,709);
