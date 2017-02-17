271b880a378350403be8b982f2081e3781ec0b70
joel.weon <joel.weon@gmail.com>
2017-02-17 10:41:23 +0900

    .

--- dbmodel/L.sql
+++ dbmodel/L.sql
@@ -1,14 +1,290 @@
--- 기업정보
+-- 기업정보 -----------------------------------
 insert into comp(cpno,cp_name) 
-    values(500,'제일기업');
+    values(500,'옥시레킷벤키저');
+    
 insert into comp(cpno,cp_name) 
-    values(501,'내일기업');
+    values(501,'유한양행');
 insert into comp(cpno,cp_name) 
-    values(502,'매일기업');
+    values(502,'유한킴벌리');
 insert into comp(cpno,cp_name) 
-    values(503,'가일기업');
+    values(503,'유한화학');
 insert into comp(cpno,cp_name) 
-    values(504,'하일기업');
+    values(504,'유한메디카');
+insert into comp(cpno,cp_name) 
+    values(505,'유한크로락스');
+insert into comp(cpno,cp_name) 
+    values(506,'한국얀센');
+insert into comp(cpno,cp_name) 
+    values(507,'엔솔테크');
+insert into comp(cpno,cp_name) 
+    values(508,'유칼릭스');
+insert into comp(cpno,cp_name) 
+    values(509,'엠지');
+    
+insert into comp(cpno,cp_name) 
+    values(510,'신세계');
+insert into comp(cpno,cp_name) 
+    values(511,'신세계건설');
+insert into comp(cpno,cp_name) 
+    values(512,'이마트');
+insert into comp(cpno,cp_name) 
+    values(513,'신세계백화점');
+insert into comp(cpno,cp_name) 
+    values(514,'신세계아이앤씨');
+insert into comp(cpno,cp_name) 
+    values(515,'광주신세계');
+insert into comp(cpno,cp_name) 
+    values(516,'신세계푸드');
+insert into comp(cpno,cp_name) 
+    values(517,'스타벅스코리아');
+insert into comp(cpno,cp_name) 
+    values(518,'신세계인터네셔널');
+insert into comp(cpno,cp_name) 
+    values(519,'조선호텔베이커리');
+insert into comp(cpno,cp_name) 
+    values(520,'신세계조선호텔');
+insert into comp(cpno,cp_name) 
+    values(521,'신세계첼시');
+
+
+insert into comp(cpno,cp_name) 
+    values(522,'아모레퍼시픽');
+insert into comp(cpno,cp_name) 
+    values(523,'아리따움');
+insert into comp(cpno,cp_name) 
+    values(524,'라네즈');
+insert into comp(cpno,cp_name) 
+    values(525,'마몽드');
+insert into comp(cpno,cp_name) 
+    values(526,'아이오페');
+insert into comp(cpno,cp_name) 
+    values(527,'롤리타램피카');
+insert into comp(cpno,cp_name) 
+    values(528,'아닉구딸');
+insert into comp(cpno,cp_name) 
+    values(529,'한율');
+insert into comp(cpno,cp_name) 
+    values(530,'헤라');
+insert into comp(cpno,cp_name) 
+    values(531,'설화수');
+insert into comp(cpno,cp_name) 
+    values(532,'리리코스');
+insert into comp(cpno,cp_name) 
+    values(533,'프리메라');
+insert into comp(cpno,cp_name) 
+    values(534,'이니스프리');
+insert into comp(cpno,cp_name) 
+    values(535,'베리떼');
+insert into comp(cpno,cp_name) 
+    values(536,'에뛰드');
+insert into comp(cpno,cp_name) 
+    values(537,'롤리타렘피카향수');
+insert into comp(cpno,cp_name) 
+    values(538,'에스쁘아');
+insert into comp(cpno,cp_name) 
+    values(539,'려');
+insert into comp(cpno,cp_name) 
+    values(540,'일리');
+insert into comp(cpno,cp_name) 
+    values(541,'미쟝센');
+insert into comp(cpno,cp_name) 
+    values(542,'해피바스');
+insert into comp(cpno,cp_name) 
+    values(543,'댄트롤샴푸');
+insert into comp(cpno,cp_name) 
+    values(544,'송염치약');
+insert into comp(cpno,cp_name) 
+    values(544,'오설록');
+insert into comp(cpno,cp_name) 
+    values(544,'바이탈뷰티');
+    
+--피앤지
+insert into comp(cpno,cp_name) 
+    values(545,'피앤지');
+insert into comp(cpno,cp_name) 
+    values(546,'페브리즈');
+insert into comp(cpno,cp_name) 
+    values(547,'다우니');
+insert into comp(cpno,cp_name) 
+    values(548,'에리얼');
+insert into comp(cpno,cp_name) 
+    values(549,'팬틴');
+insert into comp(cpno,cp_name) 
+    values(550,'헤드앤숄더');
+insert into comp(cpno,cp_name) 
+    values(551,'위스퍼');
+insert into comp(cpno,cp_name) 
+    values(552,'브라운');
+insert into comp(cpno,cp_name) 
+    values(553,'질레트');
+insert into comp(cpno,cp_name) 
+    values(554,'듀라셀');
+insert into comp(cpno,cp_name) 
+    values(555,'SK2');
+insert into comp(cpno,cp_name) 
+    values(556,'오랄비');
+insert into comp(cpno,cp_name) 
+    values(557,'팸퍼스');
+
+-- 위메프
+insert into comp(cpno,cp_name) 
+    values(558,'위메프');
+insert into comp(cpno,cp_name) 
+    values(559,'워더홀딩스');
+insert into comp(cpno,cp_name) 
+    values(560,'위메프');
+insert into comp(cpno,cp_name) 
+    values(561,'위메프');
+
+
+--기업관계 ----------------------------------------
+--유한
+insert into com_rls(parno,chino) 
+    values(501,502);
+insert into com_rls(parno,chino) 
+    values(501,503);
+insert into com_rls(parno,chino) 
+    values(501,504);
+insert into com_rls(parno,chino) 
+    values(501,505);
+insert into com_rls(parno,chino) 
+    values(501,506);
+insert into com_rls(parno,chino) 
+    values(501,507);
+insert into com_rls(parno,chino) 
+    values(501,508);
+insert into com_rls(parno,chino) 
+    values(501,509);
+
+--신세계
+insert into com_rls(parno,chino) 
+    values(510,511);
+insert into com_rls(parno,chino) 
+    values(510,512);
+insert into com_rls(parno,chino) 
+    values(510,513);
+insert into com_rls(parno,chino) 
+    values(510,514);
+insert into com_rls(parno,chino) 
+    values(510,515);
+insert into com_rls(parno,chino) 
+    values(510,516);
+insert into com_rls(parno,chino) 
+    values(510,517);
+insert into com_rls(parno,chino) 
+    values(510,518);
+insert into com_rls(parno,chino) 
+    values(510,519);
+insert into com_rls(parno,chino) 
+    values(510,520);
+insert into com_rls(parno,chino) 
+    values(510,521);
+    
+--아모레퍼시픽
+insert into com_rls(parno,chino)
+    values(522,523);
+insert into com_rls(parno,chino)
+    values(522,524);
+insert into com_rls(parno,chino)
+    values(522,525);
+insert into com_rls(parno,chino)
+    values(522,526);
+insert into com_rls(parno,chino)
+    values(522,527);
+insert into com_rls(parno,chino)
+    values(522,528);
+insert into com_rls(parno,chino)
+    values(522,529);
+insert into com_rls(parno,chino)
+    values(522,530);
+insert into com_rls(parno,chino)
+    values(522,531);
+insert into com_rls(parno,chino)
+    values(522,532);
+insert into com_rls(parno,chino)
+    values(522,533);
+insert into com_rls(parno,chino)
+    values(522,534);
+insert into com_rls(parno,chino)
+    values(522,535);
+insert into com_rls(parno,chino)
+    values(522,536);
+insert into com_rls(parno,chino)
+    values(522,537);
+insert into com_rls(parno,chino)
+    values(522,538);
+insert into com_rls(parno,chino)
+    values(522,539);
+insert into com_rls(parno,chino)
+    values(522,540);
+insert into com_rls(parno,chino)
+    values(522,541);
+insert into com_rls(parno,chino)
+    values(522,542);
+insert into com_rls(parno,chino)
+    values(522,543);
+insert into com_rls(parno,chino)
+    values(522,544);
+-- 피앤지
+insert into com_rls(parno,chino)
+    values(545,546);
+insert into com_rls(parno,chino)
+    values(545,547);
+insert into com_rls(parno,chino)
+    values(545,548);
+insert into com_rls(parno,chino)
+    values(545,549);
+insert into com_rls(parno,chino)
+    values(545,550);
+insert into com_rls(parno,chino)
+    values(545,551);
+insert into com_rls(parno,chino)
+    values(545,552);
+insert into com_rls(parno,chino)
+    values(545,553);
+insert into com_rls(parno,chino)
+    values(545,554);
+insert into com_rls(parno,chino)
+    values(545,555);
+insert into com_rls(parno,chino)
+    values(545,556);
+insert into com_rls(parno,chino)
+    values(545,557);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--불매기업관계
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 -- 공동구매
