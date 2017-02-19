3233cd2c758131cba2c86960152cb1e06aef0495
hanokjoo <okjoo.han88@gmail.com>
2017-02-19 12:21:59 +0900

    .

--- src/main/webapp/js/boycott.js
+++ src/main/webapp/js/boycott.js
@@ -1,12 +1,31 @@
 /* 썸머노트 가져오기 */
-$(document).ready(function(event) {
-  $('#summernote').summernote({
-    height: 450,                 // set editor height
-    minHeight: null,             // set minimum height of editor
-    maxHeight: null,             // set maximum height of editor
-    focus: true,                  // set focus to editable area after initializing summernote
-    lang: 'ko-KR'
-  });
+$(function(event) {
+  var device = navigator.userAgent;
+  if (device.indexOf('Mobile') != -1) {
+    // 모바일로 접속 시 툴바 제한
+    //console.log('모바일임');
+    $('#summernote').summernote({
+      height: 300,                 // set editor height
+      minHeight: null,             // set minimum height of editor
+      maxHeight: null,             // set maximum height of editor
+      focus: true,                  // set focus to editable area after initializing summernote
+      lang: 'ko-KR',
+      toolbar: [
+        ['fontsize', ['fontsize']],
+        ['para', ['paragraph']],
+        ['insert', ['picture']],
+      ]
+    });
+  } else {
+    //console.log(navigator.userAgent);
+    $('#summernote').summernote({
+      height: 450,                 // set editor height
+      minHeight: null,             // set minimum height of editor
+      maxHeight: null,             // set maximum height of editor
+      focus: true,                  // set focus to editable area after initializing summernote
+      lang: 'ko-KR'
+    });
+  }
   
   $('a#write').click(function(event) {
     var result = $('#summernote').summernote('code');
--- src/main/webapp/js/review.js
+++ src/main/webapp/js/review.js
@@ -1,4 +1,30 @@
-$(function() {
+$(function(event) {
   //select box Name로 접근하여 선택된 값 읽기
   //$("select[name=셀렉트박스name]").val();
+  var device = navigator.userAgent;
+  if (device.indexOf('Mobile') != -1) {
+    // 모바일로 접속 시 툴바 제한
+    //console.log('모바일임');
+    $('#summernote').summernote({
+      height: 300,                 // set editor height
+      minHeight: null,             // set minimum height of editor
+      maxHeight: null,             // set maximum height of editor
+      focus: true,                  // set focus to editable area after initializing summernote
+      lang: 'ko-KR',
+      toolbar: [
+        ['fontsize', ['fontsize']],
+        ['para', ['paragraph']],
+        ['insert', ['picture']],
+      ]
+    });
+  } else {
+    //console.log(navigator.userAgent);
+    $('#summernote').summernote({
+      height: 450,                 // set editor height
+      minHeight: null,             // set minimum height of editor
+      maxHeight: null,             // set maximum height of editor
+      focus: true,                  // set focus to editable area after initializing summernote
+      lang: 'ko-KR'
+    });
+  }
 });
