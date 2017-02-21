d7f501dc2b775ae8e09cd3050fe169ceed914f20
hanokjoo <okjoo.han88@gmail.com>
2017-02-21 10:05:16 +0900

    메인 외 다른 페이지 header.js로 인한 에러 발생 수정

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -3,9 +3,17 @@
  ------------------*/
 $(document).ready(function() {
   $('#wrap-header').load('./header.html');
+  $('#wrap-hidden').load('./hidden.html');
   $('#div-intro').load('./intro.html');
   $('#wrap-footer').load('./footer.html');
-  addJavascript('../js/header.js');
+  //로그인, 회원가입 창에서는 header.js 로딩하지 않음
+  if(location.href.indexOf('login') != -1 ||
+      location.href.indexOf('join') != -1) {
+    /*console.log("로그인임");*/
+  } else {
+    /*console.log("로그인 아님");*/
+    addJavascript('../js/header.js');
+  }
 });
 
 function addJavascript(jsname) {
