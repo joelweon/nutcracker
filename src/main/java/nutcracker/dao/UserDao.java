131a56a51622339e6ad10577a2787b430f13c7ab
suhee <sue.parksuhee@gmail.com>
2017-02-27 13:50:02 +0900

    sh

--- src/main/java/nutcracker/dao/UserDao.java
+++ src/main/java/nutcracker/dao/UserDao.java
@@ -1,12 +1,13 @@
 package nutcracker.dao;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import nutcracker.domain.User;
 
 public interface UserDao {
   ArrayList<User> getList() throws Exception;
-  User getOneByEmail(String email) throws Exception;
+  User getOneByEmailPassword(Map<String,String> paramMap) throws Exception;
   int countByEmail(String email) throws Exception;
   int countByNo(int userNo) throws Exception;
   int insert(User user) throws Exception;
--- src/main/webapp/mypage/mypage.js
+++ src/main/webapp/mypage/mypage.js
@@ -1,9 +1,32 @@
-$(document).ready(function() {
+jQuery(function() {
   $('#wrap-mytop').load(clientRoot + '/mypage/mytop.html');
   $('#wrap-myside').load(clientRoot + '/mypage/myside.html');
 });
 
+//로그인된 회원(member) 정보 불러오기
+$(document).ready(function() {
+  $.getJSON(serverRoot + '/auth/loginUser.json', function(ajaxResult) {
+  	$('#profile-photo').attr('src', clientRoot+'/images/user/'+ajaxResult.data.photoPath);
+  	$('#profile-name').text(ajaxResult.data.name);
+  	$('#re-email').attr('placeholder', ajaxResult.data.email);
+  	$('#re-email').attr('value', ajaxResult.data.email);
+  	return ajaxResult;
+  });
+});
+
+//활동내역 보기
 $(document).on('click', '.detail-togle', function() {
   $(".my-top-detail").toggle("1000");
+});
+
+//마이페이지 리로그인 클릭 이벤트 -> user 정보 가져오기
+$("#re-login").click(function() {
+	var param = {
+	    email: $('#re-email').val(),
+	    password: $('#re-password').val(),
+	  };
+	$.getJSON(serverRoot + '/user/detail.json', param, function(ajaxResult) {
+		location.href = clientRoot + "/mypage/mydetail.html";
+  });
 });
 
