f6a4a5a1ff847df1ed4ecc564cccef81aaa84e34
joel.weon <joel.weon@gmail.com>
2017-03-03 13:37:30 +0900

    수희누나 토스욤

--- src/main/webapp/deal/deal-write2.js
+++ src/main/webapp/deal/deal-write2.js
@@ -1,7 +1,5 @@
 $( function() {
-  $( ".datepicker" ).datepicker({
-    dateFormat:'yy-mm-dd'
-  });
+  $( ".datepicker" ).datepicker();
   
   var cnt = 2;
   $('a#add-photo').click(function (event) {
@@ -24,6 +22,9 @@ $( function() {
 newForm();
 function newForm() {
   $('#write').click(function() {
+    var a = $('input[type=file]').each(function(i, e) {
+      console.log($(this).text());
+    })
     var param = {
         title: $('#input-title').val(),
         price: $('#input-price').val(),
@@ -31,7 +32,8 @@ function newForm() {
         startDate: $('#start-date').val(),
         endDate: $('#end-date').val(),
         deliveryDate: $('#deliv-date').val(),
-        photoPath: $('#photo-img').val()
+        
+        photoPath: a
     };
     console.log(param);
     $.post(serverRoot + '/deal/add.json', param, function(ajaxResult) {
@@ -49,26 +51,25 @@ $('#input-photo').fileupload({
   sequentialUploads: true,  // 여러 개의 파일을 업로드 할 때 순서대로 요청하기.
   singleFileUploads: false, // 한 요청에 여러 개의 파일을 전송시키기. 기본은 true.
   autoUpload: true,        // 파일을 추가할 때 자동 업로딩 여부 설정. 기본은 true.
-  maxNumberOfFiles: 4,
-  limitConcurrentUploads: 3,
   disableImageResize: /Android(?!.*Chrome)|Opera/
       .test(window.navigator && navigator.userAgent), // 안드로이드와 오페라 브라우저는 크기 조정 비활성 시키기
-  previewMaxWidth: 800,   // 미리보기 이미지 너비
-  previewMaxHeight: 800,  // 미리보기 이미지 높이 
-  previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
+//  previewMaxWidth: 800,   // 미리보기 이미지 너비
+//  previewMaxHeight: 800,  // 미리보기 이미지 높이 
+//  previewCrop: true,      // 미리보기 이미지를 출력할 때 원본에서 지정된 크기로 자르기
   done: function (e, data) { // 서버에서 응답이 오면 호출된다. 각 파일 별로 호출된다.
     console.log(data.result);
       $('#photo-path').val(data.result.data[0]);
-  },
-  processalways: function(e, data) {
-    console.log('data...', data);
-    console.log('fileuploadprocessalways()...', data.files.length, data.index);
-    var img = $('#photo-img');
-    if (data.index == 0) {
-      var canvas = data.files[0].preview;
-      var dataURL = canvas.toDataURL();
-      img.attr('src', dataURL).css('width', '100px');
-      $('#photo-label').css('display', '');
-    }
   }
+//  processalways: function(e, data) {
+//    console.log('data...', data);
+//    console.log('fileuploadprocessalways()...', data.files.length, data.index);
+//    var img = $('#photo-img');
+//    if (data.index == 0) {
+//      var canvas = data.files[0].preview;
+//      var dataURL = canvas.toDataURL();
+//      img.attr('src', dataURL).css('width', '100px');
+//      $('#photo-label').css('display', '');
+//    }
+//  }
 });
+
