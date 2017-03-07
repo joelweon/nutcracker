3857907437c27d09ec8fcf691f7b518c2fa52c42
joel.weon <joel.weon@gmail.com>
2017-03-07 10:55:01 +0900

    로그인 enter + click 적용

--- src/main/webapp/auth/login.js
+++ src/main/webapp/auth/login.js
@@ -1,15 +1,27 @@
-$('#btn-login').click(function() {
-  var param = {
-    email: $('#email').val(),
-    password: $('#password').val(),
-  };
-  $.post(serverRoot + '/auth/login.json', param, function(ajaxResult) {
-    if (ajaxResult.status == "success") {
-    	window.sessionStorage.setItem('user', JSON.stringify(ajaxResult.data));
-      location.href = clientRoot + "/main.html"; 
-      return;
-    }
-    alert(ajaxResult.data);
-  }, 'json');
+/*$('#btn-login').click(function() {
+
   
 }); // click()
+
+*/
+document.querySelector('#btn-login').addEventListener('click',login);
+document.querySelector('#password').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+      login();
+    }
+});
+function login() {
+  var param = {
+      email: $('#email').val(),
+      password: $('#password').val(),
+    };
+    $.post(serverRoot + '/auth/login.json', param, function(ajaxResult) {
+      if (ajaxResult.status == "success") {
+        window.sessionStorage.setItem('user', JSON.stringify(ajaxResult.data));
+        location.href = clientRoot + "/main.html"; 
+        return;
+      }
+      alert(ajaxResult.data);
+    }, 'json');
+}
