9a81149dfb73c31c605daffddcf9549904a3f19d
suhee <sue.parksuhee@gmail.com>
2017-03-17 10:20:57 +0900

    sh

--- src/main/webapp/mypage/mypurchase.js
+++ src/main/webapp/mypage/mypurchase.js
@@ -1,11 +1,11 @@
 var users = window.sessionStorage.getItem('user');
 var memberNo = JSON.parse(users).memberNo;
 var purchaseNo;
+var newList =[];
 
 // 리스트 및 팝업 출력
 $(function(e) {
 	$.get(serverRoot + '/mypage/myPurchseHistory.json', 'memberNo='+memberNo, function(ajaxResult) {
-		if(ajaxResult.status != 'success') { console.log('myPurchseHistory.fail'); return; }
 		var list = ajaxResult.data;
 		// 구매 리스트
 		var tbody = $('.purchase-table > tbody');
@@ -13,9 +13,10 @@ $(function(e) {
     tbody.html(template({"list":list}));
     // 작성 버튼 클릭
     $(".review-btn").click(function() {
+    	$('#hugi').removeClass('active');
     	purchaseNo = $(this).attr("data-no");
+    	buttonAfterReview(purchaseNo);
     	$.get(serverRoot + '/deal/detail.json', {purchaseNo}, function(ajaxResult) {
-    		if (ajaxResult.status != 'success') { console.log('detail.fail'); return; }
     		$('.purchase-image > img').attr('src', serverRoot+'/upload/deal/'+ajaxResult.data.photoList.photoPath);
     		$('.purchase-title').text(ajaxResult.data.title);
     		$('.purchase-percent > div').text(ajaxResult.data.percent+" %");
@@ -23,7 +24,7 @@ $(function(e) {
     		$(".mask").addClass("active");
     	});
     });
-    // 마스크 끝내기
+    // 모달 닫기
     $(".close").click(function(){
     	closeModal();
     });
@@ -31,6 +32,17 @@ $(function(e) {
     	if (e.keyCode == 27) {closeModal();}
     });
 	});
+});
+
+// 후기 5자 이하일 경우 버튼 비활성화
+$('#textarea').keyup(function() {
+	if ($('#textarea').val().length >= 5) {
+		$('#hugi').addClass('active');
+		$('#hugi').removeAttr('disabled');
+	} else {
+		$('#hugi').removeClass('active');
+		$('#hugi').attr('disabled','disabled');
+	}
 });
 
 //구매 후기 등록
@@ -41,13 +53,29 @@ $('.purchase-review-button').click(function() {
 		purchaseNo : purchaseNo
 	};
 	$.post(serverRoot+'/comment/purchasecommentadd.json',param,function() {
-		$('#textarea').val("");
+		alertify.alert("구매 후기가 등록되었습니다.");
 		closeModal();
 	});
 });
 
+// 구매 등록 후 버튼 변경
+var buttonAfterReview = function(purchaseNo) {
+	$.get(serverRoot+'/comment/productcomments.json', {purchaseNo}, function(AjaxResult) {
+		if (AjaxResult.data != null) {
+			$('#writeBtn_'+purchaseNo).css('display','none');
+			$('#completeBtn_'+purchaseNo).css('display','inline-block');
+			$('#completeBtn_'+purchaseNo+' .review-btn-completed').css('display','inline-block');
+		} else {
+			$('#writeBtn_'+purchaseNo).css('display','inline-block');
+			$('#completeBtn_'+purchaseNo).css('display','none');
+			$('#completeBtn_'+purchaseNo+' .review-btn-completed').css('display','none');
+		}
+	});
+};
+
 var closeModal =function() {
 	$(".mask").removeClass("active");
+	$('#textarea').val("");
 };
 
 $(document).on('click', '.detail-togle', function() {
