4dd3992adbdcff02b284f8bf37878c7a9a9181fe
suhee <sue.parksuhee@gmail.com>
2017-03-24 10:28:08 +0900

    sh

--- src/main/webapp/detail/detail.js
+++ src/main/webapp/detail/detail.js
@@ -50,7 +50,7 @@ $(function() {
 	// 공구 정보 가져오기
 
 	$.getJSON(serverRoot + '/deal/detailByBotno.json', {boycottNo}, function(ajaxResult) {
-		if (ajaxResult.status == "sucess") {
+		if (ajaxResult.status == "success") {
 			$('.purchase-img img').attr('src', clientRoot+'/upload/deal/'+ajaxResult.data.photoList.photoPath);
 			$('.purchase-subtitle').text(ajaxResult.data.title);
 		} else {
